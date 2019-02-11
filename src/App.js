7b2faf8a8fb4096b0f4560ce3f8eae53eaf542d1
Ewa Maj <awemaj@gmail.com>
2019-02-11 09:07:12 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
-import Contact from './Contact'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Contact from './Contact';
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
-    return(
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton  color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" >
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <p>hello world</p>
-      <Contact />
+    return (
+      <div>
+<Router>
+<div>
+    <Navbar />
 
-    </div>
+    <Route exact path='/' component={App} />
+    <Route path='/contact' component={Contact} />
+
+    
+</div>
+</Router>
+       
+       
+      </div>
     )
 
   }
